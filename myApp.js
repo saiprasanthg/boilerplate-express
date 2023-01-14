78ff406e19ff0d1f0b8599e7372118e232635697
saiprasanthg <saiprasanth.guthula@sjsu.edu>
2023-01-14 11:45:16 -0800

    changes

--- myApp.js
+++ myApp.js
@@ -1,39 +1,21 @@
 let express = require('express');
 let app = express();
+var path = require('path');
+
+//console.log("Hello World");
+path = __dirname + "/public";
+app.use("/public", express.static(path));
 
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
